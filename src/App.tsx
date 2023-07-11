97d010e31a32014cdb87f082b1f343d7bd5b28bb
EagleVector <kumarsubhambili3@gmail.com>
2023-07-11 19:44:22 +0530

    Winner Code Logic

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,41 @@ function App() {
   const [state, setState] = useState(Array(9).fill(null));
   const [currentTurn, setCurrentTurn] = useState("X");
 
+  const checkWinner = (state: any[]) => {
+    const win = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6]
+    ];
+    for (let i = 0; i < win.length; i++) {
+      const [a, b, c] = win[i];
+      if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
+        return true;
+      }
+    }
+    return false;
+  }
+
   const handleBlockClick = (index: number) => {
-    const stateCopy = Array.from(state)
+    const stateCopy = Array.from(state);
+
+    if (stateCopy[index] !== null) return;
+
     stateCopy[index] = currentTurn;
+
+    // Check if someone won
+
+    const win = checkWinner(stateCopy);
+
+    if (win) {
+      alert(`${currentTurn} won the game`)
+    }
+
     setCurrentTurn(currentTurn === 'X' ? '0' : 'X');
     setState(stateCopy);
   };
